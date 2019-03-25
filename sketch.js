ff46a565b47586d20f1ab08b1d83eef281a1abe3
David Lucero <david.lucero@ttu.edu>
2019-03-25 02:52:10 -0500

    commit

--- sketch.js
+++ sketch.js
@@ -3,92 +3,131 @@ var w = window,
     e = d.documentElement,
     g = d.getElementsByTagName('body')[0],
     x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+    y = w.innerHeight || e.clientHeight || g.clientHeight;
 
 var particles = [];
-
+let mouse;
 //acts like a constructor
-function setup() { 
+function setup() {
     createCanvas(x, y);
-    for(let i = 0; i< 125; i++){
-        p = new Particle(random(0,x), random(0,y));
+    for (let i = 0; i < x/20; i++) {
+        p = new Particle(random(0, x), random(0, y));
         particles.push(p);
     }
-   
-  } 
-  
-  function draw() { 
-    background(225);
+
+
+}
+
+function draw() {
+    // background(75)
+    background(151, 127, 105);
+    background(182, 25, 36)
+
     particles.forEach((p) => {
         p.show();
         p.update();
-     
+
     })
-    // for(let i = 0; i<particles.length; i++){
-    //     if(particles[i].finished()){
-    //         particles[i].valpha = -1 * particles[i].valpha;
-    //     }
-    // }
-   
+
+
+
 }
 
 
 class Particle {
-    constructor(x,y){
-        this.x = x;
-        this.y = y;
-        this.vx = random(-1,1);
-        this.vy = random(-1,1)
+
+    constructor(x, y) {
         this.alpha = 250
-        this.valpha = 1 //random(-1,1);
-        this.r = 2.5
+        this.valpha = 1
+        this.r = 2
+        this.acceleration = createVector(0, 0)
+        this.velocity = createVector(random(-2, 2), random(-2, 2))
+        this.location = createVector(x, y);
+        this.ogV = this.velocity;
+
     }
-    show(){
+    show() {
         noStroke();
         fill(this.alpha)
-        ellipse(this.x,this.y, 2*this.r)
+        ellipse(this.location.x, this.location.y, 2 * this.r)
     }
-    update(){
-        this.x += this.vx
-        this.y += this.vy
-        //this.alpha += this.valpha;
+    update() {
+     
+        //this.acceleration = createVector(0,0)
+        this.velocity.add(this.acceleration);
+        this.location.add(this.velocity);
+        this.velocity.limit(1)
         this.alpha = 250;
+        this.nearMouse()
         this.checkEdgeCollision();
         this.intersects();
-     
+        
+
     }
-    finished(){
+    finished() {
         return this.alpha > 150 || this.alpha <= 0
     }
-    intersects(){
-        for(let i = 0; i< particles.length; i++){
+    intersects() {
+        for (let i = 0; i < particles.length; i++) {
             let p = particles[i];
-             if(!(p.x == this.x && p.y == this.y)){
-                if(dist(this.x,this.y,p.x,p.y) < ((this.r + p.r) * 10)){          
-                    stroke(0);
-                    line(this.x,this.y,p.x,p.y)
+            if (!(p.location.x == this.location.x && p.location.y == this.location.y)) {
+                if (dist(this.location.x, this.location.y, p.location.x, p.location.y) < ((this.r + p.r) * 30)) {
+                    stroke(50);
+                    line(this.location.x, this.location.y, p.location.x, p.location.y)
                     this.changeColor(0)
                     p.changeColor(0)
-                    
+
                 }
-                if(dist(this.x,this.y,p.x,p.y) < ((this.r + p.r))){
-                     this.vx*= -1;
-                     this.vy*= -1;
-                    
+                if (dist(this.location.x, this.location.y, p.x, p.y) <= ((this.r + p.r))) {
+                    this.velocity.x *= -1;
+                    this.velocity.y *= -1;
                 }
-             }
-          
+            }
+
         }
     }
-    checkEdgeCollision(){
-        if(this.x >= x || this.x <= 0){
-            this.vx *= -1;
+    nearMouse() {
+        let mouser = 100
+        let pushv = 3
+        noFill()
+        stroke(0)
+        noStroke()
+        ellipse(mouseX, mouseY, mouser*2)
+
+            if (dist(this.location.x, this.location.y, mouseX, mouseY) < (this.r + mouser)) {
+                if(this.location.x < mouseX){
+                      this.velocity.x = -pushv;
+                    // this.location.x = mouseX - mouser
+                }
+                if(this.location.x > mouseX){
+                    this.velocity.x = pushv;
+                    //this.location.x = mouseX + mouser
+
+                }
+                if(this.location.y > mouseY){
+                    this.velocity.y = pushv;
+                    //this.location.y = mouseY + mouser
+
+                }
+                if(this.location.y < mouseY){
+                    this.velocity.y = -pushv;
+                    //this.location.y = mouseY - mouser
+
+                }
+            
         }
-        if(this.y >= y || this.y <= 0){
-            this.vy *= -1;
+
+
+    }
+    checkEdgeCollision() {
+        if (this.location.x >= x || this.location.x <= 0) {
+            this.velocity.x *= random(-2,-1);
+        }
+        if (this.location.y >= y || this.location.y <= 0) {
+            this.velocity.y *= random(-2,-1);
         }
     }
-    changeColor(color){
+    changeColor(color) {
         this.alpha = color
     }
 }
